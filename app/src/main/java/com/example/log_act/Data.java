aefee3cd9ae040001caa460aa88a45b93b935d54
ilyes-ced <random_dude_233@proton.me>
2023-10-28 18:52:07 +0100

    almost finished

--- app/src/main/java/com/example/log_act/Data.java
+++ app/src/main/java/com/example/log_act/Data.java
@@ -1,5 +1,5 @@
 package com.example.log_act;
-
+import android.util.Log;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,12 +11,79 @@ public class Data extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.data);
+
+        TextView username = findViewById(R.id.username_val);
         TextView email = findViewById(R.id.email_val);
         TextView password = findViewById(R.id.password_val);
+        TextView code = findViewById(R.id.code_val);
+        TextView number = findViewById(R.id.number_val);
+
         Intent intent = getIntent();
+
+        String username_val = intent.getStringExtra("username");
         String email_val = intent.getStringExtra("email");
         String password_val = intent.getStringExtra("password");
-        email.setText(email_val);
-        password.setText(password_val);
+        String code_val = intent.getStringExtra("code");
+        String number_val = intent.getStringExtra("number");
+
+
+
+        if (intent.hasExtra("username")) {
+            if (username_val.equals("")){
+                username.setText("--blank--");
+            }else{
+                username.setText(username_val);
+            }
+        }else {
+            username.setText("NaN");
+        }
+
+
+        if (intent.hasExtra("email")) {
+            if (email_val.equals("")){
+                email.setText("--blank--");
+            }else{
+                email.setText(email_val);
+            }
+        }else {
+            email.setText("NaN");
+        }
+
+
+        if (intent.hasExtra("password")) {
+            if (password_val.equals("")){
+                password.setText("--blank--");
+            }else{
+                password.setText(password_val);
+            }
+        }else {
+            password.setText("NaN");
+        }
+
+
+
+        if (intent.hasExtra("code")) {
+            if (code_val.equals("")){
+                code.setText("--blank--");
+            }else{
+                code.setText(code_val);
+            }
+        }else {
+            code.setText("NaN");
+        }
+
+
+        if (intent.hasExtra("number")) {
+            if (number_val.equals("")){
+                number.setText("--blank--");
+            }else{
+                number.setText(number_val);
+            }
+        }else {
+            number.setText("NaN");
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/log_act/Register.java
+++ app/src/main/java/com/example/log_act/Register.java
@@ -2,6 +2,11 @@ package com.example.log_act;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +16,53 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.phone_num_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.country_codes,
+                android.R.layout.simple_spinner_item
+        );
+        spinner.setSelection(0, true);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+
+        Button button = findViewById(R.id.confirm_register);
+        EditText username = findViewById(R.id.username);
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+
+        Spinner phone_num_container = findViewById(R.id.phone_num_spinner);
+        EditText phone_num = findViewById(R.id.phone_num);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, Data.class);
+                intent.putExtra("username", username.getText().toString());
+                intent.putExtra("email", email.getText().toString());
+                intent.putExtra("password",  password.getText().toString());
+                intent.putExtra("code", phone_num_container.getSelectedItem().toString());
+                intent.putExtra("number", phone_num.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
+
+        TextView go_login = findViewById(R.id.go_login);
+        go_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
